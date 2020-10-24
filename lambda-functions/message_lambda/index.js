4c05dbe6403a1eecc92bdb79e7e4138d9f69313e
Rahulm2310 <rahulmohata2310@gmail.com>
2020-10-24 13:08:15 +0530

    messaging

--- lambda-functions/message_lambda/index.js
+++ lambda-functions/message_lambda/index.js
@@ -5,8 +5,16 @@ DB();
 exports.handler = async (event) => {
   try {
     switch (event.type) {
-      case "getMessages":
-        return await Message.find();
+      // case "getMessages":
+      //   return await Message.find();
+      case "getDistinctListingsFromMessages":
+        return await Message.distinct("listingId");
+      case "getDriverOwnerMessagesByListingId":
+        return await Message.find({ $and:[{ listingId:event.arguments.listingId},{driverId:event.arguments.driverId},{ownerId:event.arguments.ownerId}]});
+      case "getDriverMessages":
+        return await Message.find({driverId:event.arguments.driverId}); 
+      case "getListingMessages":
+        return await Message.find({listingId:event.arguments.listingId});   
       case "createMessage":
         return await Message.create(event.arguments);
       default:
--- lambda-functions/message_lambda/utils/messageModel.js
+++ lambda-functions/message_lambda/utils/messageModel.js
@@ -1,13 +1,53 @@
 const mongoose = require("mongoose");
 
+// const messageSchema = new mongoose.Schema({
+//   body: String,
+//   createdAt: {
+//     type: Date,
+//     default: Date.now(),
+//   },
+//   handle: String,
+// });
+
 const messageSchema = new mongoose.Schema({
-  body: String,
-  createdAt: {
+  body:{
+    type:String,
+    required:true
+  },
+  listingId:{
+    type:String,
+    required:true
+  },
+  listingAddress:{
+    type:String,
+    required:true
+  },
+  ownerId:{
+    type:String,
+    required:true
+  },
+  ownerName:{
+    type:String,
+    required:true
+  },
+  driverId:{
+    type:String,
+    required:true
+  },
+  driverName:{
+    type:String,
+    required:true
+  },
+  senderName:{
+    type:String,
+    required:true
+  },
+  createdAt:{
     type: Date,
     default: Date.now(),
-  },
-  handle: String,
+  }
 });
+
 
 const Message = mongoose.model("Message", messageSchema);
 
