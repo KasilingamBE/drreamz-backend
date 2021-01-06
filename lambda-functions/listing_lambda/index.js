b5fe0cf983bce06b4251150e71f3b6dfe1753055
vivekvt <vivek78643@gmail.com>
2021-01-07 01:22:04 +0530

    fixed schedule bug fix

--- lambda-functions/listing_lambda/index.js
+++ lambda-functions/listing_lambda/index.js
@@ -7,6 +7,8 @@ DB();
 exports.handler = async (event) => {
   try {
     let tempFilter = {};
+    let startDate = null;
+    let endDate = null;
     switch (event.type) {
       case 'getListing':
         return await Listing.findById(event.arguments.id);
@@ -68,6 +70,30 @@ exports.handler = async (event) => {
           Date.parse(event.arguments.end) - Date.parse(event.arguments.start);
         const diffFromNow =
           Date.parse(event.arguments.start) - Date.parse(new Date());
+        const {
+          startDay,
+          startHour,
+          startMinute,
+          endDay,
+          endHour,
+          endMinute,
+        } = event.arguments;
+
+        const isStartDayMonday = startDay === 1 ? true : 'null';
+        const isEndDayMonday = endDay === 1 ? true : 'null';
+        const isStartDayTuesday = startDay === 2 ? true : 'null';
+        const isEndDayTuesday = endDay === 2 ? true : 'null';
+        const isStartDayWednesday = startDay === 3 ? true : 'null';
+        const isEndDayWednesday = endDay === 3 ? true : 'null';
+        const isStartDayThursday = startDay === 4 ? true : 'null';
+        const isEndDayThursday = endDay === 4 ? true : 'null';
+        const isStartDayFriday = startDay === 5 ? true : 'null';
+        const isEndDayFriday = endDay === 5 ? true : 'null';
+        const isStartDaySaturday = startDay === 6 ? true : 'null';
+        const isEndDaySaturday = endDay === 6 ? true : 'null';
+        const isStartDaySunday = startDay === 0 ? true : 'null';
+        const isEndDaySunday = endDay === 0 ? true : 'null';
+
         return await Listing.aggregate([
           {
             $geoNear: {
@@ -89,6 +115,359 @@ exports.handler = async (event) => {
               $or: [
                 { 'spaceAvailable.hasNoticeTime': false },
                 { 'spaceAvailable.noticeTime.value': { $lte: diffFromNow } },
+              ],
+              $or: [
+                {
+                  'spaceAvailable.scheduleType': '24hours',
+                },
+                {
+                  $and: [
+                    {
+                      'spaceAvailable.scheduleType': 'fixed',
+                    },
+                    {
+                      $or: [
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.monday.isActive': isStartDayMonday,
+                              $or: [
+                                {
+                                  'spaceAvailable.monday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.monday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.monday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.monday.isActive': isEndDayMonday,
+                              $or: [
+                                {
+                                  'spaceAvailable.monday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.monday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.monday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.tuesday.isActive': isStartDayTuesday,
+                              $or: [
+                                {
+                                  'spaceAvailable.tuesday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.tuesday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.tuesday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.tuesday.isActive': isEndDayTuesday,
+                              $or: [
+                                {
+                                  'spaceAvailable.tuesday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.tuesday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.tuesday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.wednesday.isActive': isStartDayWednesday,
+                              $or: [
+                                {
+                                  'spaceAvailable.wednesday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.wednesday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.wednesday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.wednesday.isActive': isEndDayWednesday,
+                              $or: [
+                                {
+                                  'spaceAvailable.wednesday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.wednesday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.wednesday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.thursday.isActive': isStartDayThursday,
+                              $or: [
+                                {
+                                  'spaceAvailable.thursday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.thursday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.thursday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.thursday.isActive': isEndDayThursday,
+                              $or: [
+                                {
+                                  'spaceAvailable.thursday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.thursday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.thursday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.friday.isActive': isStartDayFriday,
+                              $or: [
+                                {
+                                  'spaceAvailable.friday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.friday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.friday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.friday.isActive': isEndDayFriday,
+                              $or: [
+                                {
+                                  'spaceAvailable.friday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.friday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.friday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.saturday.isActive': isStartDaySaturday,
+                              $or: [
+                                {
+                                  'spaceAvailable.saturday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.saturday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.saturday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.saturday.isActive': isEndDaySaturday,
+                              $or: [
+                                {
+                                  'spaceAvailable.saturday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.saturday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.saturday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          $and: [
+                            {
+                              'spaceAvailable.sunday.isActive': isStartDaySunday,
+                              $or: [
+                                {
+                                  'spaceAvailable.sunday.startHour': {
+                                    $lt: startHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.sunday.startHour': startHour,
+                                    },
+                                    {
+                                      'spaceAvailable.sunday.startMinute': {
+                                        $lte: startMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              'spaceAvailable.sunday.isActive': isEndDaySunday,
+                              $or: [
+                                {
+                                  'spaceAvailable.sunday.endHour': {
+                                    $gt: endHour,
+                                  },
+                                },
+                                {
+                                  $and: [
+                                    {
+                                      'spaceAvailable.sunday.endHour': endHour,
+                                    },
+                                    {
+                                      'spaceAvailable.sunday.endMinute': {
+                                        $gte: endMinute,
+                                      },
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
               ],
             },
           },
