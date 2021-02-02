66ead00a1d6c5b0b5476c44e47638c8a8e162ce8
Bilal <bilalmomin39@gmail.com>
2021-02-02 21:09:36 +0530

    SES check

--- lambda-functions/homepage_lambda/index.js
+++ lambda-functions/homepage_lambda/index.js
@@ -18,7 +18,7 @@ exports.handler = async (event) => {
     // callback(null, "Submitting Email");
     //                                   }
   const tempData = {
-    emails: [event.arguments.email],
+    emails: [event.arguments.Email_id],
     subject: "Test Email",
     message: "Testing SES on backend",
   };
