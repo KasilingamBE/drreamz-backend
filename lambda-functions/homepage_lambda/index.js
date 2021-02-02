0255599700ab3678080d0d33a98bac0563755be4
Bilal <bilalmomin39@gmail.com>
2021-02-02 21:05:17 +0530

    SES check

--- lambda-functions/homepage_lambda/index.js
+++ lambda-functions/homepage_lambda/index.js
@@ -8,24 +8,24 @@ DB();
 
 exports.handler = async (event) => {
   try {
-    switch (event.type) {
-      case "updateHomepageEmailTask":
-        return await HomePage.create({Email_id: event.arguments.Email_id,
-                                        });
+    // switch (event.type) {
+    //   case "updateHomepageEmailTask":
+    //     return await HomePage.create({Email_id: event.arguments.Email_id,
+    //                                     });
     
 
   
-    callback(null, "Submitting Email");
- 
+    // callback(null, "Submitting Email");
+    //                                   }
   const tempData = {
     emails: [event.arguments.email],
     subject: "Test Email",
     message: "Testing SES on backend",
   };
     return await mailer(tempData);
-  default:
-      return null;
-    }
+  // default:
+  //     return null;
+    // }
   
   }
   catch (error) {
